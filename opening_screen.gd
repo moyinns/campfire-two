7e5cd710c03f1e4eead7e7eb405a181013d3e5cf
Moyinoluwa Akinnawonu <moyinaki@gmail.com>
2026-02-28 11:53:39 +0000

    added opening screen
    no working buttons yet :(

---
+++ opening_screen.gd
@@ -0,0 +1,24 @@
+extends Node2D
+@onready var opening_screen: Node2D = $"."
+@onready var settings_button: Button = $settings_button
+@onready var game_title: Label = $game_title
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_play_button_pressed() -> void:
+	pass # Replace with function body.
+func _on_settings_button_pressed() -> void:
+	pass # Replace with function body.
+func _on_quit_button_pressed() -> void:
+	pass # Replace with function body.
