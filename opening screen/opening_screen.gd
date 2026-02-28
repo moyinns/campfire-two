d0779d7d07d047a50669d2bad5995b64ad37613f
Moyinoluwa Akinnawonu <moyinaki@gmail.com>
2026-02-28 17:09:19 +0000

    added the boss battle scene

--- opening screen/opening_screen.gd
+++ opening screen/opening_screen.gd
@@ -17,8 +17,8 @@ func _process(delta: float) -> void:
 
 
 func _on_play_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://deciding_screen/deciding_screen.tscn")
 func _on_settings_button_pressed() -> void:
 	pass # Replace with function body.
 func _on_quit_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().quit()
