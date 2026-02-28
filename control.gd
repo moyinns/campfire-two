7e0f690fe4ec120529b56f9b9512dd655e7ee299
daniellefubara-boop <daniellefubara@gmail.com>
2026-02-28 14:50:40 +0000

    added char buttons

---
+++ control.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@onready var characterpink: TextureButton = $Characterpink
+@onready var characterblack: TextureButton = $Characterblack
+@onready var characterpurple: TextureButton = $Characterpurple
+@onready var characterred: TextureButton = $Characterred
+@onready var charactergrey: TextureButton = $Charactergrey
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+
+
+func _on_characterred_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_charactergrey_pressed() -> void:
+	pass # Replace with function body.
