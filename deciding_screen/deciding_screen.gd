1acb451056d90977260533afd0656a2afb896d85
kitanaderogba-star <kitan.aderogba@gmail.com>
2026-02-28 15:34:43 +0000

    updates

--- deciding_screen/deciding_screen.gd
+++ deciding_screen/deciding_screen.gd
@@ -1,6 +1,19 @@
 extends Node2D
 
-
+var list = [["Karen Blobsworth", "52" , 
+"Middle manager at a logistics company. Known for her passive agressive Post-it note campaigns in the office kitchen. Has strong opinions about the communal microwave and is not afraid to share them in pointed emails",
+"She made company-wide emails saying, Can everyone PLEASE label their lunches, fourteen times in one year "],
+			["Henry Octagon" , "55" , 
+			"A bold, audacious, rude nobleman with a passion for power, feasting, war and getting remarried at an alarming rate. Yet somehow not a one his fault. Owned around 60 palaces and still complained bout drafts", 
+			"Ordered all the monastries destroyed because he wanted money to go to war. He lost the wars."],
+			["Charle Emileao" , "20",
+			"An internallionaly famous young woman who became famous for dancing on social media. Somehow this turned into a media empire, a shoe deal, and a family reality show. However, she seems genuiely nice, which confuses everyone",
+			"Made parents worldwide discover TikTok"],
+			["Tonald Drump" , "78" ,
+			"A very audacious orange businessman turned politician with an unmistakeble tan and haircut. He built towers, sent tweets aboout being the best including humble and gen AI of his enemies", "Told people to drink bleach to avoid getting sick during pandemic"],
+			["Oluwaeren Okafor", "19",
+			"A brooding, intense young man form Lagos who discovered a terribel secret about the world and took it extremely personally. Trained for years, had great abs, and a plan so complicated he forgot which side he was on by the end. His friends tried to stop him. he didn't stop",
+			"Decided the solution to a deeply complex geopolitical problem was to personaly flatten everything and then explain his reasoning while people where still on fire"]]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
